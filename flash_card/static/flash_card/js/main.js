9d1a8a4db321fead77eaab21946033dba62cb27c
Patrick <pjlloyd100@gmail.com>
2016-07-14 21:40:06 -0500

    Card number increases properly when a new row is made. Closes #10

--- flash_card/static/flash_card/js/main.js
+++ flash_card/static/flash_card/js/main.js
@@ -1,9 +1,17 @@
 function newCard() {
+    // Subtract one from card_count because of empty card template
+    var card_count = $('.row.card').length - 1;
+    console.log(card_count);
     var empty_card = $('#empty_row').clone();
+
     // remove empty_row attribute
-    empty_card.removeAttr('id')
+    empty_card.removeAttr('id');
+
     // add new_row attribute
     empty_card.attr('class', 'row card new_row');
+
+    var counter = empty_card.find('.counter');
+    counter.html(card_count + 1);
     $('#set-formset').append(empty_card[0].outerHTML);
 }
 

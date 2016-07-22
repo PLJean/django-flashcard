1d5a41f8763ee9a783676d0b551d043aa5a7115c
Patrick <pjlloyd100@gmail.com>
2016-07-22 14:44:08 -0500

    Fixed bug to make new card (row) save when created. Closes #11

--- flash_card/static/flash_card/js/main.js
+++ flash_card/static/flash_card/js/main.js
@@ -24,11 +24,25 @@ function newCard() {
     // add new_row attribute
     new_card.attr('class', 'row card container-fluid new_row');
 
+    // Changing card counter
     var counter = new_card.find('.counter');
     var card_count = $('.row.card').length - 1;
     card_count += 1;
-    console.log(card_count);
     counter.html(card_count);
+
+    // Changing card front id and nameE
+    var front = new_card.find('#id_form-0-front');
+    front.attr('id',   'id_form-' + card_count + '-front');
+    front.attr('name', 'form-' + card_count + '-front');
+    console.log(front.attr('id'));
+    console.log(front.attr('name'));
+
+    // Changing card back id and name
+    var back = new_card.find('#id_form-0-back');
+    back.attr('id',   'id_form-' + card_count + '-back');
+    back.attr('name', 'form-' + card_count + '-back');
+    console.log(back.attr('id'));
+    console.log(back.attr('name'));
     $('#set-formset').append(new_card[0].outerHTML);
 }
 

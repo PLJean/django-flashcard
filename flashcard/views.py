3db215cc5c861d172a35036e918356c21c30f4e9
Patrick <pjlloyd100@gmail.com>
2016-08-10 17:10:17 -0500

    Learn implemented

--- flashcard/views.py
+++ flashcard/views.py
@@ -163,9 +163,10 @@ def flip(request, set_id):
 
 
 def learn(request, set_id):
-    flashcards = serializers.serialize("json", Set.objects.get(id=set_id).card_set.all())
+    flashcards_data = Set.objects.get(id=set_id)
+    flashcards = serializers.serialize("json", flashcards_data.card_set.all())
     return render(request, 'flashcard/learn.html', {
-        'cards': flashcards, 'set_id': set_id,
+        'title': flashcards_data.name, 'cards': flashcards, 'set_id': set_id,
     })
 
 
